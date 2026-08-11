e77be06921995640696863a54fada7c730af8828
Ayushman Behera <ayushman93370@gmail.com>
2026-08-11 15:29:36 +0530

    I added the unique id and search feature

--- main.py
+++ main.py
@@ -1,10 +1,12 @@
 import json
-# import myModules
+import myModules
 
-# Id = myModules.idGenerator(7)
 
-with open("studentData.json","r") as file:
-    data = json.load(file)
+try:
+    with open("studentData.json","r") as file:
+        data = json.load(file)
+except (FileNotFoundError , json.JSONDecodeError):
+    data = []
 
 # student class
 class Student:
@@ -21,21 +23,65 @@ class Student:
             "age":self.age,
             "qualification":self.qualificaiton
         }
-    
+
+# Check if the given object is already in the json file or not
+studentNameList = [info["name"] for info in data if "name" in info]
+def check_if_exits(obj):
+    if obj.name in studentNameList:
+        print(f"{obj.name} is already exsits.")
+        return False
+    else:
+        return True
 
 
-new_id = len(data)+1
-
-
-Litu = Student(new_id,"Ayushman",20,"Graduation")
-
+# making unique ids
+studentIds = [info["id"] for info in data if "id" in info]
+def generate_unique_id():
+    while True:
+        new_id = myModules.idGenerator(7)
+        if new_id not in studentIds:
+            return new_id
+        
 
 # send the new_student to json 
-def sentTOJsonfile(obj):
-    dictData = obj.to_dict()
-    data.append(dictData)
+def saveInDatabase(obj):
+    global studentIds , studentNameList
+    if not check_if_exits(obj):
+        return
+    
+    new_id = generate_unique_id()
+    obj.id = new_id
+    
+    studentIds.append(new_id)
+    studentNameList.append(obj.name)
+
+    data.append(obj.to_dict())
     with open("studentData.json","w")as file:
         json.dump(data,file,indent=4)
 
-vicky = Student(new_id,"Niranjan",20,"graduation")
-sentTOJsonfile(vicky)
+
+# Show all the students who are present in the database
+def displayStudents():
+    for info in data:
+        print(info["name"],info["age"])
+
+
+# # making new studetns
+Litu = Student(None,"Ayushman",20,"Graduation")
+saveInDatabase(Litu)
+vicky = Student(None,"Niranjan",20,"Graduation")
+saveInDatabase(vicky)
+babul = Student(None,"Sambit",21,"Diploma")
+saveInDatabase(babul)
+
+# # Search using unique ids
+def searchStudentbyId(student_id):
+    realId = student_id.upper()
+    if len(student_id) != 7:
+        print("given Id is invalid.")
+    for info in data:
+        if realId == info["id"]:
+            print(f"| name : {info["name"]},| age : {info["age"]},| qualification : {info["qualification"]} |")
+            return
+    print("Student with id doesn't exists")
+searchStudentbyId("l16ti40")
